e5627785bbaafd00391e8af9a22154fe0bbe0c31
Justin <vien_justin@hotmail.com>
2021-12-13 19:39:15 -0500

    tweaked navigation styles, added scroll behaviour

--- navbar.js
+++ navbar.js
@@ -1,6 +1,7 @@
 // //query selectors
 const navigationMobile = document.querySelector('.navigation__mobile');
 const hamburgerNav = document.querySelector('.fa-bars');
+const navigationMain = document.querySelector('.navigation');
 
 // //Function to close mobile navigation. 
 const closeNav = function() {
@@ -10,4 +11,11 @@ const closeNav = function() {
 // //Event Listeners
 hamburgerNav.addEventListener('click', closeNav);
 
-
+//Changing the color of navbar if window is scrolled past initial position.
+window.addEventListener('scroll', function(){
+    if(this.scrollY > 0) {
+        navigationMain.classList.add('navigation__color');
+    } else {
+        navigationMain.classList.remove('navigation__color');
+    }
+})
