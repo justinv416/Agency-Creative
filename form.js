9f39b56a9af9eea79ba831f6465786f03a99b885
Justin <vien_justin@hotmail.com>
2021-12-13 21:27:37 -0500

    added inital functionality for blog comments

--- form.js
+++ form.js
@@ -1,11 +1,61 @@
-// const form = document.querySelector('#blog__form');
-// const commentName = document.querySelector('#comment__form--name')
+const form = document.querySelector('#blog__form');
+const commentName = document.querySelector('#comment__form--name');
+const commentText = document.querySelector('#comment__textarea')
 
-// form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     submitComment();
-// });
+const submitComment = function() {
+    console.log(commentName.value)
+    console.log(commentText.value)
+};
 
-// const submitComment = function() {
-//     console.log(commentName)
+// function createComment() {
+//     const commentContainer = document.createElement('div');
+//     const imageContainer = document.createElement('div');
+//     const commenterText = document.createElement('div');
+//     const profileImage = document.createElement('img');
+//     const commenterName = document.createElement('h3');
+//     const commentParagraph = document.createElement('p');
+//     // const commentDate = document.createElement('p');
+//     profileImage.src = './assets/generic-profile.png';
+//     imageContainer.appendChild(profileImage);
+//     commenterName.textContent = commentName.value;
+//     commentParagraph.textContent = commentText.value;
+//     commentText.appendChild(commenterName);
+//     commentText.appendChild(commenterText);
+//     commentContainer.appendChild(imageContainer);
+//     commentContainer.appendChild(commentText);
+//     document.querySelector('#comment').appendChild(commentContainer)
 // }
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const commentContainer = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const commenterText = document.createElement('div');
+    const profileImage = document.createElement('img');
+    const commenterName = document.createElement('h3');
+    const commentParagraph = document.createElement('p');
+    // const commentDate = document.createElement('p');
+    profileImage.src = './assets/generic-profile.png';
+    imageContainer.appendChild(profileImage);
+    commenterName.textContent = commentName.value;
+    commentParagraph.textContent = commentText.value;
+    commenterText.appendChild(commenterName);
+    commenterText.appendChild(commentParagraph);
+    commenterText.classList.add('comment__container')
+    commentContainer.appendChild(imageContainer);
+    commentContainer.appendChild(commenterText);
+    commentContainer.classList.add('comment')
+    document.querySelector('#comments').appendChild(commentContainer)
+});
+
+{/* <div class="comment">
+                <div class="">
+                    <img src="./assets/blog-7.jpeg" alt="A profile picture of a smiling man in a white shirt.">
+                </div>
+                <div class="comment__container">
+                    <h3 class="comment__name">Daniel Vandaft <span><a href="#"> - reply</a></span></h3>
+                    <p class="comment__text">Pitch agile development business plan stelth channels prototype mass market
+                        product management value proposition startup strategy.</p>
+                    <p class="comment__date">Jul 17, 2015 at 15 hours ago</p>
+                </div>
+            </div> */}
--- navbar.js
+++ navbar.js
@@ -1,15 +1,16 @@
 // //query selectors
-const navigationMobile = document.querySelector('.navigation__mobile');
-const hamburgerNav = document.querySelector('.fa-bars');
 const navigationMain = document.querySelector('.navigation');
 
 // //Function to close mobile navigation. 
 const closeNav = function() {
-    navigationMobile.classList.toggle('hideNav');
+    document.querySelector('.navigation__mobile').classList.toggle('hideNav');
+    document.querySelector('.fa-bars').classList.add('fa-times')
+    document.querySelector('.fa-times').classList.add('fa-bars')
 };
 
 // //Event Listeners
-hamburgerNav.addEventListener('click', closeNav);
+document.querySelector('.fa-bars').addEventListener('click', closeNav);
+// document.querySelector('.fa-times').addEventListener('click', closeNav);
 
 //Changing the color of navbar if window is scrolled past initial position.
 window.addEventListener('scroll', function(){
@@ -18,4 +19,20 @@ window.addEventListener('scroll', function(){
     } else {
         navigationMain.classList.remove('navigation__color');
     }
-})
+});
+
+{/* <i class="fas fa-times"></i> */}
+
+// function updateToDo(iElement) {
+//   iElement.classList.remove('fa-square');
+//   iElement.classList.add('fa-check-square');
+// }
+
+// ulElement.addEventListener("click", function(event) {
+
+//   if (event.target.tagName === 'I') {
+//     // tasks to perform
+//     updateToDo(event.target);
+//   }
+  
+// });
